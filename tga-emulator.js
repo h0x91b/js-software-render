77e2540edb5e83a4a7b790dc4dac20617cd4afe7
ars <arseniy@emi.bz>
2017-04-26 12:31:59 +0300

    [=]

--- tga-emulator.js
+++ tga-emulator.js
@@ -52,10 +52,10 @@
 	
 	class TGAColor {
 		constructor(r, g, b, a) {
-			this.r = r
-			this.g = g
-			this.b = b
-			this.a = a
+			this.r = r|0
+			this.g = g|0
+			this.b = b|0
+			this.a = a|0
 		}
 	}
 	
